1ad04b79f75c7dcbddd14ac3e39df32c0ee4c71f
yarin-zhang <yarinzhang@outlook.com>
2025-06-01 21:23:35 +0800

    2025-06-01-解决 Electron-builder 打包问题

--- vite.config.js
+++ vite.config.js
@@ -10,9 +10,9 @@ const config = defineConfig({
     root: Path.join(__dirname, 'src', 'renderer'),
     publicDir: 'public',
     main: {
-    // 调整 envPrefix 以解决 Electron + Prisma 构建问题
-    // 参考：https://github.com/prisma/prisma/discussions/21027
-      envPrefix: 'M_VITE_'
+        // 调整 envPrefix 以解决 Electron + Prisma 构建问题
+        // 参考：https://github.com/prisma/prisma/discussions/21027
+        envPrefix: 'M_VITE_'
     },
     server: {
         port: 8080,
@@ -21,6 +21,9 @@ const config = defineConfig({
     build: {
         outDir: Path.join(__dirname, 'build', 'renderer'),
         emptyOutDir: true,
+        rollupOptions: {
+            external: ['@prisma/client', '.prisma/client/default'],
+        },
     },
     plugins: [vuePlugin()],
 });
