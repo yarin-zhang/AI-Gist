eee13945053888929bdb5d0bcaecbd2845c36f26
yarin-zhang <yarinzhang@outlook.com>
2025-07-01 16:43:35 +0800

    2025-07-01-转为 JSON 版本翻译文件，移除 TS

--- src/renderer/i18n/index.ts
+++ src/renderer/i18n/index.ts
@@ -1,6 +1,6 @@
 import { createI18n } from 'vue-i18n'
-import zhCN from './locales/zh-CN'
-import enUS from './locales/en-US'
+import zhCN from './locales/zh-CN.json'
+import enUS from './locales/en-US.json'
 
 const i18n = createI18n({
   legacy: false, // 使用 Composition API 模式
